a0a1593465ceef59ccf9cbe27a9901d15fbf2c17
Eryl Kenner <eryl.kenner@gmail.com>
2018-11-10 12:47:25 -0800

    Added rotational physics, player, and planet scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
+    Planet[] planets;
 
-    // Use this for initialization
+
+    Rigidbody2D body;
+    Planet planet;
+    float radius;
+
     void Start()
     {
+        planets = FindObjectsOfType<Planet>();
 
+        body = GetComponent<Rigidbody2D>();
+        planet = getClosestPlanet();
+        radius = RotationalPhysics.GetRadius(body, planet.transform.position);
+
+        body.velocity = new Vector2(0, 20);
+        body.velocity = RotationalPhysics.OnlyTangentialVelocity(body, planet.transform.position);
+        //Debug.Log("pos: " + body.position + " vel: " + body.velocity + " planet: " + planet.transform.position + " radius: " + radius);
+        //Debug.Break();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            planet = null;
+            radius = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            planet = getClosestPlanet();
+            radius = RotationalPhysics.GetRadius(body, planet.transform.position);
+            body.velocity = RotationalPhysics.OnlyTangentialVelocity(body, planet.transform.position);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (planet != null)
+        {
+            RotationalPhysics.RotateAroundPoint(body, planet.transform.position, radius);
+        }
+    }
+
+
+    //----------------------------------------------------------------------------------------------
+
+    Planet getClosestPlanet()
+    {
+        float shortestDistance = Mathf.Infinity;
+        Planet closest = null;
+
+        foreach (Planet cur in planets)
+        {
+            float dist = Vector2.Distance(cur.transform.position, body.position);
+            if (dist < shortestDistance)
+            {
+                shortestDistance = dist;
+                closest = cur;
+            }
+        }
+        return closest;
     }
 }
---
+++ Assets/Scripts/RotationalPhysics.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RotationalPhysics
+{
+
+    public static void RotateAroundPoint(Rigidbody2D body, Vector2 centerPoint, float radius)
+    {
+        Vector2 distance = body.position - centerPoint;
+        float currentAngle = Mathf.Atan2(distance.y, distance.x);
+        float rotationDirection = -Mathf.Sign(Vector2.Dot(new Vector2(distance.y, -distance.x), body.velocity));
+        float newAngle = currentAngle + (body.velocity.magnitude / radius) * rotationDirection * Time.fixedDeltaTime;
+
+        Vector2 newPosition = centerPoint + radius * new Vector2(Mathf.Cos(newAngle), Mathf.Sin(newAngle));
+        body.velocity = body.velocity.magnitude * (newPosition - (Vector2)body.transform.position).normalized;
+    }
+
+    public static Vector2 OnlyTangentialVelocity(Rigidbody2D body, Vector2 centerPoint)
+    {
+        Vector2 curPosition = (Vector2)body.transform.position;
+        Vector2 distanceVector = curPosition - centerPoint;
+        Vector2 tangentVector = new Vector2(distanceVector.y, -distanceVector.x).normalized;
+        return Vector2.Dot(body.velocity, tangentVector) * tangentVector;
+    }
+
+    public static float GetRadius(Rigidbody2D body, Vector2 centerPoint)
+    {
+        return Vector2.Distance(body.transform.position, centerPoint);
+    }
+}
