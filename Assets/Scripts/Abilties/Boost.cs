cf4d5dcbdd92c8ce5e2b1ed483e6c40783c688f5
kylebrain <kylebrain@gmail.com>
2018-11-10 15:51:10 -0800

    Added controller controls to the Player class.

--- Assets/Scripts/Abilties/Boost.cs
+++ Assets/Scripts/Abilties/Boost.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boost : Ability {
+public class Boost : Ability
+{
 
-    protected override void DerivedUpdate() { }
+
 
     public float speedIncrease;
+
+    public float overSpeedDeceleration;
+
+    protected override void DerivedUpdate()
+    {  
+    }
+
+    private void FixedUpdate()
+    {
+        if (player.speed > player.maxSpeed)
+        {
+            float newSpeed = player.speed - overSpeedDeceleration * Time.fixedDeltaTime;
+            newSpeed = Mathf.Clamp(newSpeed, player.maxSpeed, Mathf.Infinity);
+            player.speed = newSpeed;
+        }
+    }
 
     protected override void Function()
     {
