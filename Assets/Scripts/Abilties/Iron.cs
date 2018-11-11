876c2c327b22b3b02024a11bece543b0c7a9dd4e
kylebrain <kylebrain@gmail.com>
2018-11-11 14:40:33 -0800

    Changed texture of iron powerup.

--- Assets/Scripts/Abilties/Iron.cs
+++ Assets/Scripts/Abilties/Iron.cs
@@ -7,8 +7,11 @@ public class Iron : Ability {
 
     public float Duration = 2f;
     public float IncreasedMass = 10f;
+    //public Color IronColor;
+    public Sprite IronSprite;
     public Color IronColor;
 
+    private Sprite originalSprite;
     private Color originalColor;
 
     private float orignalMass;
@@ -22,12 +25,15 @@ public class Iron : Ability {
 
     IEnumerator SetIron()
     {
+        originalSprite = player.GetComponent<SpriteRenderer>().sprite;
         originalColor = player.GetComponent<SpriteRenderer>().color;
+        player.GetComponent<SpriteRenderer>().sprite = IronSprite;
         player.GetComponent<SpriteRenderer>().color = IronColor;
         orignalMass = player.Body.mass;
         player.Body.mass = IncreasedMass;
         yield return new WaitForSeconds(Duration);
         player.Body.mass = orignalMass;
+        player.GetComponent<SpriteRenderer>().sprite = originalSprite;
         player.GetComponent<SpriteRenderer>().color = originalColor;
     }
 }
