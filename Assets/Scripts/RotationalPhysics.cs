de9465cfc70be245903b74ccf6e9085a34dcaafd
Eryl Kenner <eryl.kenner@gmail.com>
2018-11-10 17:23:37 -0800

    Fixed a bug that caused players to get stuck to planets. Fixed a bug that caused players to reverse directions

--- Assets/Scripts/RotationalPhysics.cs
+++ Assets/Scripts/RotationalPhysics.cs
@@ -9,22 +9,36 @@ public static class RotationalPhysics
     {
         radius = Mathf.Clamp(radius, minDistance, Mathf.Infinity);
         Vector2 distance = body.position - centerPoint;
+        if (distance.magnitude + speed * Time.fixedDeltaTime >= radius)
+        {
+            float currentAngle = Mathf.Atan2(distance.y, distance.x);
+            float rotationDirection = -Mathf.Sign(Vector2.Dot(new Vector2(distance.y, -distance.x), body.velocity));
+            if (rotationDirection == 0)
+            {
+                rotationDirection = 1;
+            }
+            float deltaAngle = Mathf.Acos((distance.magnitude * distance.magnitude + radius * radius - Mathf.Pow(speed * Time.fixedDeltaTime, 2)) / (2 * distance.magnitude * radius));
+            float newAngle = currentAngle + deltaAngle * rotationDirection;
 
-        float currentAngle = Mathf.Atan2(distance.y, distance.x);
-        float rotationDirection = -Mathf.Sign(Vector2.Dot(new Vector2(distance.y, -distance.x), body.velocity));
-        float deltaAngle = Mathf.Acos((distance.magnitude * distance.magnitude + radius * radius - Mathf.Pow(speed * Time.fixedDeltaTime, 2)) / (2 * distance.magnitude * radius));
-        float newAngle = currentAngle + deltaAngle * rotationDirection;
-
-        Vector2 newPosition = centerPoint + radius * new Vector2(Mathf.Cos(newAngle), Mathf.Sin(newAngle));
-        body.velocity = speed * (newPosition - (Vector2)body.transform.position).normalized;
+            Vector2 newPosition = centerPoint + radius * new Vector2(Mathf.Cos(newAngle), Mathf.Sin(newAngle));
+            body.velocity = speed * (newPosition - (Vector2)body.transform.position).normalized;
+        }
+        else
+        {
+            Vector2 tangentVelocity = new Vector2(distance.y, -distance.x);
+            body.velocity = speed * (tangentVelocity.normalized + distance.normalized).normalized;
+        }
     }
 
-    public static Vector2 OnlyTangentialVelocity(Rigidbody2D body, Vector2 centerPoint)
+    public static Vector2 ConvertToTangentialVelocity(Rigidbody2D body, Vector2 centerPoint)
     {
-        Vector2 curPosition = (Vector2)body.transform.position;
-        Vector2 distanceVector = curPosition - centerPoint;
-        Vector2 tangentVector = new Vector2(distanceVector.y, -distanceVector.x).normalized;
-        //return Vector2.Dot(body.velocity, tangentVector) * tangentVector;
+        Vector2 distanceVector = body.position - centerPoint;
+        float rotationDirection = Mathf.Sign(Vector2.Dot(new Vector2(distanceVector.y, -distanceVector.x), body.velocity));
+        if (rotationDirection == 0)
+        {
+            rotationDirection = 1;
+        }
+        Vector2 tangentVector = new Vector2(distanceVector.y, -distanceVector.x).normalized * rotationDirection;
         return body.velocity.magnitude * tangentVector;
     }
 
