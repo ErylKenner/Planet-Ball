6d9ddd11434789e970ce08a1d0becdfdef5ad9e7
Eryl Kenner <eryl.kenner@gmail.com>
2021-06-01 12:25:38 -0700

    Last commit of backup

---
+++ Assets/Scripts/Game/Graphics/PauseScreenItem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseScreenItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public bool IsResume;
+    public bool IsOptions;
+    public bool IsQuit;
+
+    private Animation anim;
+
+    public delegate void RestartScene();
+    public static event RestartScene OnRestartScene;
+
+    void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.Play("MenuItemSlideForward");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.Play("MenuItemSlideReverse");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (IsResume)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+        else if (IsOptions)
+        {
+
+        }
+        else if (IsQuit)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            Debug.LogError("Error. This button is not registered in MainMenuItem. Button text: " + GetComponent<Text>().text);
+        }
+    }
+}
