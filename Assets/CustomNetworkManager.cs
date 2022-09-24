75a61477b0245162a951729aa2f55d56eb7fcebe
Kyle Brainard <kbrainard@nevada.unr.edu>
2022-09-24 12:50:57 -0700

    feat: Add steam lobby code

---
+++ Assets/CustomNetworkManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CustomNetworkManager : NetworkManager 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
