2bb76d8ba10296034c48cffd6b809aa14dbae39c
Kyle Brainard <kbrainard@nevada.unr.edu>
2022-10-03 22:43:22 -0700

    feat: Add correction metrics

--- Assets/Scripts/Networking/NetcodeManager.cs
+++ Assets/Scripts/Networking/NetcodeManager.cs
@@ -35,6 +35,8 @@ public class NetcodeManager : NetworkBehaviour
     public static uint client_last_received_state_tick;
     private Queue<StateMessage> client_state_msgs;
 
+    int last_correction_tick;
+
     // server specific
     public uint server_snapshot_rate; // Set by user (default 1)
     [SerializeField]
@@ -45,12 +47,20 @@ public class NetcodeManager : NetworkBehaviour
     public uint server_tick_number;
     private uint server_tick_accumulator;
 
+    #endregion
+
+    #region Profiler Metrics
+
+    // Ticks
     public static readonly ProfilerCategory NetcodeCategory = ProfilerCategory.Scripts;
     public static readonly ProfilerCounter<uint> ServerTickCounter =
         new ProfilerCounter<uint>(NetcodeCategory, "Server Tick", ProfilerMarkerDataUnit.Count);
 
     public static readonly ProfilerCounter<uint> ClientTickCounter =
         new ProfilerCounter<uint>(NetcodeCategory, "Client Tick", ProfilerMarkerDataUnit.Count);
+
+    public static readonly ProfilerCounter<uint> ClientLastReceivedTickCounter =
+    new ProfilerCounter<uint>(NetcodeCategory, "Client Last Received Tick", ProfilerMarkerDataUnit.Count);
 
     public static readonly ProfilerCounter<uint> PlayerTickCounter1 =
         new ProfilerCounter<uint>(NetcodeCategory, "Player 1 Tick", ProfilerMarkerDataUnit.Count);
@@ -61,6 +71,15 @@ public class NetcodeManager : NetworkBehaviour
     public static readonly ProfilerCounter<uint>[] PlayerTickCounters = new ProfilerCounter<uint>[] { PlayerTickCounter1, PlayerTickCounter2 };
     public static List<(uint, ProfilerCounter<uint>)> NetIdToPlayerTickCounter = new List<(uint, ProfilerCounter<uint>)>();
 
+    // Correction Metrics
+    public static readonly ProfilerCounter<float> PostionErrorMetric =
+        new ProfilerCounter<float>(NetcodeCategory, "Position Error", ProfilerMarkerDataUnit.Count);
+
+    public static readonly ProfilerCounter<float> PostionCorrectionMetric =
+        new ProfilerCounter<float>(NetcodeCategory, "Position Correction", ProfilerMarkerDataUnit.Count);
+
+    public static readonly ProfilerCounter<float> PostionCorrectionTick =
+        new ProfilerCounter<float>(NetcodeCategory, "Position Correction Tick", ProfilerMarkerDataUnit.Count);
     #endregion
 
     #region Start and Update
@@ -97,6 +116,7 @@ public class NetcodeManager : NetworkBehaviour
     {
         ServerTickCounter.Sample(server_tick_number % NetcodeManager.c_client_buffer_size);
         ClientTickCounter.Sample(client_tick_number % NetcodeManager.c_client_buffer_size);
+        ClientLastReceivedTickCounter.Sample(client_last_received_state_tick % NetcodeManager.c_client_buffer_size);
     }
 
     #region Client
@@ -120,7 +140,7 @@ public class NetcodeManager : NetworkBehaviour
             }
         }
 
-        // On perform correction on a client only
+        // Perform correction on a client only
         if (!isClientOnly) { return; }
 
         Dictionary<uint, NetcodeObject> netIdToNetcodeObject = new Dictionary<uint, NetcodeObject>();
@@ -143,11 +163,14 @@ public class NetcodeManager : NetworkBehaviour
             Dictionary<uint, State> netIdToState = new Dictionary<uint, State>();
 
             client_last_received_state_tick = state_msg.tick_number;
+
             uint buffer_slot = state_msg.tick_number % c_client_buffer_size;
 
             // TODO: Smartly correct instead of correction on any
             List<Vector2> position_errors = new List<Vector2>();
             List<float> rotation_errors = new List<float>();
+
+            float playerPositionError = 0f;
             foreach (State state in state_msg.states)
             {
                 netIdToState.Add(state.netId, state);
@@ -155,6 +178,12 @@ public class NetcodeManager : NetworkBehaviour
                 //Debug.Log(currentObject.gameObject.name);
 
                 Vector2 position_error = state.state.position - currentObject.client_state_buffer[buffer_slot].position;
+
+                if(currentObject.isLocalPlayer)
+                {
+                    playerPositionError = position_error.magnitude;
+                }
+
                 //Debug.Log(position_error);
                 position_errors.Add(position_error);
                 float rotation_error = state.state.rotation - currentObject.client_state_buffer[buffer_slot].rotation;
@@ -170,16 +199,24 @@ public class NetcodeManager : NetworkBehaviour
 
             if (doCorrection)
             {
-                var offending = position_errors.Select((error, index) => new { error, index }).Where(error => error.error.sqrMagnitude > positionCorrectionMargin);
-                foreach (var offend in offending)
-                {
-                    Debug.Log(netIdToNetcodeObject[state_msg.states[offend.index].netId] + ": " + offend.error.sqrMagnitude);
-                }
+                //var offending = position_errors.Select((error, index) => new { error, index }).Where(error => error.error.sqrMagnitude > positionCorrectionMargin);
+                //foreach (var offend in offending)
+                //{
+                //    Debug.Log(netIdToNetcodeObject[state_msg.states[offend.index].netId] + ": " + offend.error.sqrMagnitude);
+                //}
+
+                PostionErrorMetric.Sample(playerPositionError);
+                last_correction_tick = (int)client_tick_number;
 
                 ClientPerformCorrection(netcodeObjects,
                                         state_msg,
                                         netIdToState,
                                         dt);
+            }
+            else
+            {
+                //Debug.Log("Not correcting");
+
             }
 
             // Smoothing
@@ -200,11 +237,13 @@ public class NetcodeManager : NetworkBehaviour
             }
 
         }
+
+        PostionCorrectionTick.Sample(last_correction_tick);
     }
 
     private static void ClientPerformCorrection(NetcodeObject[] netcodeObjects, StateMessage state_msg, Dictionary<uint, State> netIdToState, float dt)
     {
-        Debug.Log("Correcting for error at tick " + state_msg.tick_number + " (rewinding " + (client_tick_number - state_msg.tick_number) + " ticks)");
+        //Debug.Log("Correcting for error at tick " + state_msg.tick_number + " (rewinding " + (client_tick_number - state_msg.tick_number) + " ticks)");
         Dictionary<NetcodeObject, Vector2> previousPositions = new Dictionary<NetcodeObject, Vector2>();
         Dictionary<NetcodeObject, float> previousRotations = new Dictionary<NetcodeObject, float>();
 
@@ -212,9 +251,13 @@ public class NetcodeManager : NetworkBehaviour
         {
             Rigidbody2D netcodeRigidbody2D = netcodeObject.GetComponent<Rigidbody2D>();
             // capture the current predicted pos for smoothing
-            Vector2 prev_pos = netcodeRigidbody2D.position + netcodeObject.client_error.position;
+
+            // TODO: Add back error values when smoothing is implemented
+            //Vector2 prev_pos = netcodeRigidbody2D.position + netcodeObject.client_error.position;
+            Vector2 prev_pos = netcodeRigidbody2D.position;
             previousPositions.Add(netcodeObject, prev_pos);
-            float prev_rot = netcodeRigidbody2D.rotation * netcodeObject.client_error.rotation;
+            //float prev_rot = netcodeRigidbody2D.rotation + netcodeObject.client_error.rotation;
+            float prev_rot = netcodeRigidbody2D.rotation;
             previousRotations.Add(netcodeObject, prev_rot);
             State currentState = netIdToState[netcodeObject.netId];
 
@@ -256,6 +299,12 @@ public class NetcodeManager : NetworkBehaviour
             Rigidbody2D netcodeRigidbody2D = netcodeObject.GetComponent<Rigidbody2D>();
             Vector2 prev_pos = previousPositions[netcodeObject];
             float prev_rot = previousRotations[netcodeObject];
+
+            if(netcodeObject.isLocalPlayer)
+            {
+                PostionCorrectionMetric.Sample((prev_pos - netcodeRigidbody2D.position).magnitude);
+            }
+
             // if more than 2ms apart, just snap
             if ((prev_pos - netcodeRigidbody2D.position).sqrMagnitude >= 4.0f)
             {
@@ -351,6 +400,10 @@ public class NetcodeManager : NetworkBehaviour
         if (netcodePlayers.Length > 0)
         {
             uint new_server_tick_number = netcodePlayers.Min(player => player.server_tick_number);
+
+            // TODO: Switch to a fixed progression rate
+            // TODO: If we missed an input, wait a frame to see if it comes in
+
             for (int i = (int)server_tick_number; i < new_server_tick_number; ++i)
             {
 
@@ -397,6 +450,7 @@ public class NetcodeManager : NetworkBehaviour
     [ClientRpc]
     void RpcQueueClientState(StateMessage state_msg)
     {
+        //Debug.Log($"Received state {state_msg.tick_number}");
         client_state_msgs.Enqueue(state_msg);
     }
 
