15148ac3dc5342f22e9d1dc2205b166951195f7d
Kyle Brainard <kbrainard@nevada.unr.edu>
2022-10-07 21:23:38 -0700

    feat: Encapsulate scripts into Scripts assemnbly and add hello world Edit mode unit test

---
+++ Assets/Tests/EditMode/ServerTests.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ServerTests
+{
+    [Test]
+    public void ServerTestsSimplePasses()
+    {
+        Assert.True(true);
+    }
+}
