f8d68663e740a01a8aea8c33aa3ba246af79e46b
Kyle Brainard <kbrainard@nevada.unr.edu>
2022-10-31 21:54:45 -0700

    Add goal scoring text

--- Assets/Scripts/Game/GoalScored.cs
+++ Assets/Scripts/Game/GoalScored.cs
@@ -6,22 +6,38 @@ using UnityEngine;
 public class GoalScored : NetworkBehaviour
 {
     public string Name;
+    public Color teamColor;
+    public TMPro.TextMeshProUGUI text;
+
+    private float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                timer = 0;
+                text.gameObject.SetActive(false);
+            }
+        }
     }
 
     [ClientRpc]
     public void RpcPrintName(string name)
     {
         Debug.Log(Name);
+        text.color = teamColor;
+        text.gameObject.SetActive(true);
+        timer = 3;
+
     }
     
 
@@ -39,7 +55,7 @@ public class GoalScored : NetworkBehaviour
             // Disable 
 
 
-            //RpcPrintName(Name);
+            RpcPrintName(Name);
             //ball.RpcReset();
             
             //Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
