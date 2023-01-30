46cdf400ecbf2523b58cb050dcbe09406017dbd1
Kyle Brainard <kbrainard@nevada.unr.edu>
2023-01-29 17:00:55 -0800

    feat: Add FPS counter

---
+++ Assets/Scripts/Debug/FPSDisplay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+ 
+public class FPSDisplay : MonoBehaviour {
+	public TextMeshProUGUI FpsText;
+	
+	public float pollingTime = 0.5f;
+	private float time;
+	private int frameCount;
+ 
+	void Update() {
+		// Update time.
+		time += Time.deltaTime;
+
+		// Count this frame.
+		frameCount++;
+
+		if (time >= pollingTime) {
+			// Update frame rate.
+			int frameRate = Mathf.RoundToInt((float)frameCount / time);
+			FpsText.text = frameRate.ToString();
+
+			// Reset time and frame count.
+			time -= pollingTime;
+			frameCount = 0;
+		}
+	}
+}
